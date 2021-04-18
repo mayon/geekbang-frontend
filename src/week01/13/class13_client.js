99b4b90a59c5332874c77b8acf238a0515d0cecd
mayon <frontender@126.com>
2021-04-19 07:30:12 +0800

    feat(week01): update.

--- src/week01/13/class13_client.js
+++ src/week01/13/class13_client.js
@@ -59,6 +59,21 @@ class Request {
 
 class ResponseParser {
     constructor(){
+        this.WAITING_STATUS_LINE = 0;
+        this.WAITING_STATUS_LINE_END = 1;
+        this.WAITING_HEADER_NAME = 2;
+        this.WAITING_HEADER_SPACE = 3;
+        this.WAITING_HEADER_VALUE = 4;
+        this.WAITING_HEADER_LINE_END = 5;
+        this.WAITING_HEADER_BLOCK_END = 6;
+        this.WAITING_BODY = 7;
+
+        this.current = this.WAITING_STATUS_LINE;
+        this.statusLine = "";
+        this.headers = {};
+        this.headerName = "";
+        this.headerValue = "";
+        this.bodyParser = null;
     }
     receive(string){
         for(let i = 0; i < string.length; i++) {
@@ -66,7 +81,27 @@ class ResponseParser {
         }
     }
     receiveChar(char){
-        
+        if(this.current === this.WAITING_HEADER_LINE) {
+            if(char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END;
+            } else {
+                this.statusLine += char;
+            }
+        } else if(this.current === this.WAITING_HEADER_LINE_END) {
+            if(char === '\n') {
+                this.current = this.WAITING_HEADER_NAME;
+            }
+        } else if(this.current === this.WAITING_HEADER_NAME) {
+            if(char === ':') {
+                this.current = this.WAITING_HEADER_SPACE;
+            } else if(char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END;
+            } else {
+                this.headerName += char;
+            }
+        } else if(this.current === this.WAITING_HEADER_VALUE) {
+            if()
+        }
     }
 }
 
